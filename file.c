0b9068aa9e5f0179ec68f99fa3f1bd01e58ad895
brunda222 <brundabandaru222@gmail.com>
2026-01-28 11:57:49 +0530

    Add files via upload

---
+++ file.c
@@ -0,0 +1,33 @@
+#include<sys/stat.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main()
+{
+   int i=0;
+   int f1,f2;
+   char c,strin[100];
+   /*open file for writing*/
+   f1= open("data_BRUNDA",O_RDWR/O_CREAT,O_TRUNC,0644);
+   /*read input from key board*/
+   while((c=getchar())!='\n')
+   {
+   strin[i++]=c;
+   }
+   strin[i]='\0';
+   /*write data into file*/
+   write(f1,strin,i);
+   close(f1);  
+   /*open file for reading*/
+   f2=open("data_BRUNDA",O_RDONLY);
+   /*read data from file*/
+   read (f2,strin,i);
+   strin[i] ='\0';
+   /*display file content*/
+   printf("\n data read from file:\n %s\n",strin);
+   close(f2);
+   return 0;
+   }
+   
+
